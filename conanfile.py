1535e3c1aaaf7b5c38809f664e8ae7b89c6e627b
root <root@project2.tbgpwxgrjzmexkakowkih3pezg.tx.internal.cloudapp.net>
2025-07-10 12:56:39 +0000

    cartest

---
+++ conanfile.py
@@ -0,0 +1,12 @@
+from conan import ConanFile
+
+class AutowarePlatform(ConanFile):
+    name = "autoware_platform"
+    version = "1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = (
+        "gtest/1.14.0",
+        "boost/1.83.0"
+    )
+    generators = "CMakeDeps", "CMakeToolchain"
+
